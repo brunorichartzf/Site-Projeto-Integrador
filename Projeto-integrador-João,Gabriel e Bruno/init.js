075837585f0910258db50c0ed94178372594b145
BrunoRF <50593937+brunorichartzf@users.noreply.github.com>
2020-11-19 14:20:10 -0300

    Add files via upload

---
+++ Projeto-integrador-João,Gabriel e Bruno/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAXZlgXk9EqCRWQXuajdTnwko4XQnOHRsU",
+  authDomain: "site-projetointegrador2.firebaseapp.com",
+  databaseURL: "https://site-projetointegrador2.firebaseio.com",
+  projectId: "site-projetointegrador2",
+  storageBucket: "site-projetointegrador2.appspot.com",
+  messagingSenderId: "963179052250",
+  appId: "1:963179052250:web:3efe6b8bb9cee048f339e3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
